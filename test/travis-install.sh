8075ed632bb090e015f6dbee252f313aafb4f98f
nhenriksen <shireham@gmail.com>
2018-04-06 11:15:58 -0700

    Troubleshooting paprika install in .travis.yml

--- test/travis-install.sh
+++ test/travis-install.sh
@@ -14,8 +14,9 @@ else
     fi
     bash miniconda.sh -b -p $HOME/miniconda
     export PATH="$HOME/miniconda/bin:$PATH"
+    conda config --add channels omnia --add channels conda-forge
     conda create -y -n myenv python=$TRAVIS_PYTHON_VERSION
-    conda install -y -n myenv numpy pytest pytest-cov codecov
+    conda install -y -n myenv numpy pymbar pytest pytest-cov codecov
     conda install -y -n myenv ambertools=17.0 -c http://ambermd.org/downloads/ambertools/conda/
 fi
 
